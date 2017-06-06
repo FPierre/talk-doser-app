bbc4c83cfee49dae8f011601df5fdd21d3a330ce
FPierre <pflauder@gmail.com>
2017-06-06 20:33:29 +0200

    Affichage des personnes concernées par la conversation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,68 @@
 <template>
   <div id='app'>
-    <router-view></router-view>
+    <header>
+      <h1>TalkDoser</h1>
+      <span>Conversation entre {{ peopleList }}</span>
+    </header>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+    <footer>Footer</footer>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'app',
+  computed: {
+    ...mapGetters([
+      'getPeople'
+    ]),
+    peopleList () {
+      return Object.keys(this.getPeople).join(', ')
+    }
+  },
   created () {
     this.$store.dispatch('getTalkData')
   }
 }
 </script>
+
+<style>
+/*#7f9eb2*/
+/*#77919d*/
+
+body {
+  background-color: #0d3446;
+  color: #d8dfe2;
+  font-size: 1rem;
+  font-family: 'Hind', sans-serif;
+}
+
+header {
+  background-color: #fff;
+  color: #0d3446;
+  grid-area: header;
+}
+
+main {
+  grid-area: main;
+}
+
+footer {
+  grid-area: footer;
+}
+
+.container {
+  grid-template-columns: 50px;
+  grid-template-rows: auto;
+  grid-template-areas:
+    'header'
+    'main'
+    'footer';
+}
+</style>
